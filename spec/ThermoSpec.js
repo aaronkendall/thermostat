64c35db5651735e1345fdb6b08b7289f28d35cea
Aaron <aarongkendall@gmail.com>
2015-09-29 15:54:46 +0100

    Min and max temperatures added

--- spec/ThermoSpec.js
+++ spec/ThermoSpec.js
@@ -9,15 +9,36 @@ describe("Thermostat", function() {
     expect(thermostat.temp).toEqual(20);
   });
 
-  it("should be possible to increase temperature by 1 degree", function(){
+  it("should be possible to increase temperature by 1 degree", function() {
     thermostat.temp_up();
     expect(thermostat.temp).toEqual(21);
-    console.log(thermostat)
   });
 
-  it("should be possible to decrease temperature by 1 degree", function(){
+  it("should be possible to decrease temperature by 1 degree", function() {
     thermostat.temp_down();
     expect(thermostat.temp).toEqual(19);
-    console.log(thermostat)
+  });
+
+  it("should reset to 20 degrees when the reset method is called", function() {
+    thermostat.temp_up();
+    thermostat.temp_reset();
+    expect(thermostat.temp).toEqual(20);
+  });
+
+  it("should not let temperature go below 10 degrees", function() {
+    thermostat.temp_change(10);
+    thermostat.temp_down();
+    expect(thermostat.temp).toEqual(10);
+  });
+
+  it("should not let temperature go above 32 degrees", function() {
+    thermostat.temp_change(32);
+    thermostat.temp_up();
+    expect(thermostat.temp).toEqual(32);
+  });
+
+  it("should change temp", function() {
+    thermostat.temp_change(10);
+    expect(thermostat.temp).toEqual(10);
   });
 });
--- src/Thermo.js
+++ src/Thermo.js
@@ -2,10 +2,40 @@ function Thermostat() {
   this.temp = 20;
 };
 
-Thermostat.prototype.temp_up = function() {
-  this.temp++;
-};
+  Thermostat.prototype.temp_up = function() {
+    if(this.temp_max()){
+      return;
+    }
+    else{
+      this.temp++;
+    };
+  };
 
-Thermostat.prototype.temp_down = function() {
-  this.temp--;
-};
+  Thermostat.prototype.temp_down = function() {
+    if(this.temp_min()){
+      return;
+    }
+    else {
+      this.temp--;
+    };
+  };
+
+  Thermostat.prototype.temp_reset = function() {
+    this.temp = 20;
+  };
+
+  Thermostat.prototype.temp_min = function() {
+    if(this.temp == 10){
+      return true;
+    };
+  };
+
+  Thermostat.prototype.temp_max = function() {
+    if(this.temp == 32){
+      return true;
+    };
+  };
+
+  Thermostat.prototype.temp_change = function(degrees) {
+    this.temp = degrees;
+  };
